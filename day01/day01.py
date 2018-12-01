084ef60672e2a4a75737f3dd3aae897d9cef5b6b
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-01 22:52:24 +0100

    solve part 2 of day01 problem

--- day01/day01.py
+++ day01/day01.py
@@ -2,6 +2,17 @@
 
 import os.path
 
+def first_duplicate(changes):
+    frequency, i = 0, 0
+    seen_frequencies = set()
+
+    while True:
+        seen_frequencies.add(frequency)
+        frequency += changes[i % len(changes)]
+        if frequency in seen_frequencies:
+            return frequency
+        i += 1
+
 if __name__ == '__main__':
     filename = "day01_changes.txt"
 
@@ -10,3 +21,4 @@ if __name__ == '__main__':
     else:
         changes = list(map(int, open(filename).read().split()))
         print("PART ONE:", sum(changes))
+        print("PART TWO:", first_duplicate(changes))
