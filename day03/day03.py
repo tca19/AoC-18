7c302ac60b4c99445e4f87c5d756f7476ec1e8b9
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-03 22:22:30 +0100

    refactor file parsing into its own function

--- day03/day03.py
+++ day03/day03.py
@@ -4,33 +4,34 @@ import os.path
 
 grid = [ [0 for _ in range(1000)] for _ in range(1000) ]
 
+def parse_file(filename):
+    data = []
+    with open(filename) as f:
+        for line in f:
+            id, _, pos, size = line.split()
+            x, y = pos[:-1].split(',')
+            width, height = size.split('x')
+            x      = int(x)
+            y      = int(y)
+            width  = int(width)
+            height = int(height)
+            data.append((id, x, y, width, height))
+    return data
+
 def size_overlap(lines):
     total_overlap = 0
     for line in lines:
-        _, _, pos, size = line.split()
-        x, y = pos[:-1].split(',')
-        width, height = size.split('x')
-        x      = int(x)
-        y      = int(y)
-        width  = int(width)
-        height = int(height)
+        id, x, y, width, height = line
         for i in range(x, x+width):
             for j in range(y, y+height):
                 if grid[i][j] == 1:
                     total_overlap += 1
                 grid[i][j] += 1
-
     return total_overlap
 
 def id_not_overlap(lines):
     for line in lines:
-        id, _, pos, size = line.split()
-        x, y = pos[:-1].split(',')
-        width, height = size.split('x')
-        x      = int(x)
-        y      = int(y)
-        width  = int(width)
-        height = int(height)
+        id, x, y, width, height = line
         does_overlap = False
         for i in range(x, x+width):
             for j in range(y, y+height):
@@ -44,7 +45,7 @@ if __name__ == '__main__':
     if not os.path.exists(filename):
         print("ERROR. Name your input file as:", filename)
     else:
-        lines = open(filename).read().splitlines()
+        lines = parse_file(filename)
         part_1 = size_overlap(lines)
         print("PART ONE:", part_1)
         part_2 = id_not_overlap(lines)
