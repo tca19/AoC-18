752cc84a949bd5e327f9039aa0568a6aba619faf
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-03 22:03:32 +0100

    solve part 2 of day03

--- day03/day03.py
+++ day03/day03.py
@@ -22,6 +22,23 @@ def size_overlap(lines):
 
     return total_overlap
 
+def id_not_overlap(lines):
+    for line in lines:
+        id, _, pos, size = line.split()
+        x, y = pos[:-1].split(',')
+        width, height = size.split('x')
+        x      = int(x)
+        y      = int(y)
+        width  = int(width)
+        height = int(height)
+        does_overlap = False
+        for i in range(x, x+width):
+            for j in range(y, y+height):
+                if grid[i][j] > 1:
+                    does_overlap = True
+        if not does_overlap:
+            return id[1:]
+
 if __name__ == '__main__':
     filename = "day03_claims.txt"
     if not os.path.exists(filename):
@@ -30,3 +47,5 @@ if __name__ == '__main__':
         lines = open(filename).read().splitlines()
         part_1 = size_overlap(lines)
         print("PART ONE:", part_1)
+        part_2 = id_not_overlap(lines)
+        print("PART TWO:", part_2)
