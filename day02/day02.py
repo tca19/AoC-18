da60b76141ff8c091bfa5101ad7dac65a28b5cf4
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-04 17:34:16 +0100

    add day02 problem description + part 1,2 statements

--- day02/day02.py
+++ day02/day02.py
@@ -24,6 +24,14 @@ def common_correct_letters(list_id):
             if distance == 1:
                 return "".join([a for a,b in zip(s1, s2) if a == b])
 
+# Input is a list of box IDs like bpacnmelhhzpygfsjoxtvkwuor,
+# biacnmelnizqygfsjoctvkwudr... One ID per line.
+#
+# Part 1: compute the product between the number of IDs which have exactly two
+#         of any letter and the number of IDs which have exactly tree of any
+#         letter.
+# Part 2: find the two IDs which only differs by 1 char. Print the common
+#         letters of these IDs.
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit("usage: ./day02.py INPUT_FILE")
