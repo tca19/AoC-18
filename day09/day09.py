f84a5269e50a9a71c66dc99d12ec48e728199eec
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-10 22:41:48 +0100

    optimize solution of day09 (use doubly linked list)

--- day09/day09.py
+++ day09/day09.py
@@ -3,6 +3,50 @@
 import os
 import sys
 
+# class representing a node in a doubly linked list
+class Node:
+    def __init__(self, val):
+        self.prev = None
+        self.next = None
+        self.val  = val
+
+# insert a new node with value `val` AFTER node `self`. Return the new node.
+def insert(n, val): # n <-> m becomes n <-> new <-> m
+    new = Node(val)
+    new.prev    = n
+    new.next    = n.next
+    n.next.prev = new
+    n.next      = new
+    return new
+
+# move back by `offset` positions from `current` node in the list.
+def move_back(current, offset):
+    for _ in range(offset):
+        current = current.prev
+    return current
+
+# Return the score of the wininng player of the marble game. Use a doubly linked
+# list to represent the circle where the marbles are inserted.
+def winning_score(n_players, n_marbles):
+    scores = [0 for _ in range(n_players)]
+    current_player = 0
+    current_node = Node(0)
+    current_node.next = current_node
+    current_node.prev = current_node
+    for m in range(1, n_marbles+1):
+        if m%23 == 0:
+            scores[current_player] += m
+            current_node = move_back(current_node, 7)
+            scores[current_player] += current_node.val
+            current_node.prev.next = current_node.next
+            current_node.next.prev = current_node.prev
+            current_node = current_node.next
+        else:
+            current_node = insert(current_node.next, m)
+        current_player = (current_player + 1) % n_players
+
+    return max(scores)
+
 # Input file is the number of players and the value of the last marble. The
 # players play the marble game. The game consists of inserting in turn the
 # marbles (starting with the marble with value 1, then the marble with value 2,
@@ -25,3 +69,4 @@ if __name__ == '__main__':
     line = open(filename).read().split()
     n_players = int(line[0])
     n_marbles = int(line[-2])
+    print("PART ONE:", winning_score(n_players, n_marbles))
