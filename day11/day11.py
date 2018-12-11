78150380c109ef2ae05d3e87409a1ae19db2d90d
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-11 18:25:55 +0100

    solve part 1 of day11

--- day11/day11.py
+++ day11/day11.py
@@ -5,12 +5,27 @@ import sys
 
 SERIAL_NUMBER = 8979
 
+# Return the top-left coordinates of the 3x3 square with the largest power.
+def coordinates_max_power(grid):
+    max_power   = -10**9
+    best_square = (-1, -1)
+    for x in range(1, 299):
+        for y in range(1, 299):
+            s = grid[x-1][y-1] + grid[x][y-1] + grid[x+1][y-1] +\
+                grid[x-1][y]   + grid[x][y]   + grid[x+1][y]   +\
+                grid[x-1][y-1] + grid[x][y+1] + grid[x+1][y+1]
+            if s > max_power:
+                max_power = s
+                best_square = (x, y)
+
+    return "{},{}".format(*best_square)
+
 # A 300x300 grid represents the power level of fuel cells. Each cell has a value
 # that depends on its x,y coordinates and the serial number (the file input).
 #
 # Part 1: find the top-left coordinates of the 3 x 3 square that has the largest
 #         total power (sum of the fuel power of its 9 cells).
-# Part 2: find the top-left coordinattes and the size k of the k x k square that
+# Part 2: find the top-left coordinates and the size k of the k x k square that
 #         has the largest total power (sum of the fuel power of all its cells).
 if __name__ == '__main__':
     if len(sys.argv) > 2:
@@ -20,3 +35,8 @@ if __name__ == '__main__':
         if not os.path.exists(filename):
             sys.exit("error: {} does not exist.".format(filename))
         SERIAL_NUMBER = int(open(filename).read())
+
+    # init cells with fuel power formula
+    grid = [ [ ((((X+10)*Y + SERIAL_NUMBER) * (X+10)) // 100) % 10 - 5 \
+               for Y in range(1, 301) ] for X in range(1, 301) ]
+    print("PART ONE:", coordinates_max_power(grid))
