2a960f692daad40936ba740f8870d9fc76dbb532
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-12 19:28:36 +0100

    regroup solutions of part 1 and 2 into a single function

--- day11/day11.py
+++ day11/day11.py
@@ -2,54 +2,46 @@
 
 import os
 import sys
+from copy import deepcopy
 
 SERIAL_NUMBER = 8979
 
-# Return the top-left coordinates of the 3x3 square with the largest power.
-def coordinates_max_power(grid):
-    max_power   = -10**9
-    best_square = (-1, -1)
-    for x in range(1, 299):
-        for y in range(1, 299):
-            s = grid[x-1][y-1] + grid[x][y-1] + grid[x+1][y-1] +\
-                grid[x-1][y]   + grid[x][y]   + grid[x+1][y]   +\
-                grid[x-1][y-1] + grid[x][y+1] + grid[x+1][y+1]
-            if s > max_power:
-                max_power = s
-                best_square = (x, y)
-
-    return "{},{}".format(*best_square)
-
 # Return the top-left coordinates and the size of the square with the largest
-# total power (the sum of fuel power of all its cells).
-def coordinates_max_power_any(grid):
-    # build the summed-area table of grid
+# total power (the sum of fuel power of all its cells). If square_size is given,
+# only look at squares of size square_size x square_size.
+def coordinates_max_power(grid, square_size=None):
+    # build the summed-area table `s_grid` of `grid`
     L = len(grid)
+    s_grid = deepcopy(grid) # nested lists, need deepcopy to have a full copy
     for x in range(1, L):
-        grid[x][0] += grid[x-1][0] # cumulative sum of first column
-        grid[0][x] += grid[0][x-1] # cumulative sum of first row
+        s_grid[x][0] += s_grid[x-1][0] # cumulative sum of first column
+        s_grid[0][x] += s_grid[0][x-1] # cumulative sum of first row
     for x in range(1, L):
         for y in range(1, L):
             # each cell is the sum of all the values above and on the left
-            grid[x][y] += grid[x][y-1] + grid[x-1][y] - grid[x-1][y-1]
+            s_grid[x][y] += s_grid[x][y-1] + s_grid[x-1][y] - s_grid[x-1][y-1]
 
     max_power   = -10**9
     best_square = (-1, -1)
     best_size   = -1
-    for size in range(1, 301):
+    sizes       = [square_size] if square_size else range(1, L+1)
+    for size in sizes:
         for x in range(1, 300-size):
             for y in range(1, 300-size):
                 # top-left is (x, y); bottom-right is (x + size-1, y + size-1)
-                s = grid[x + size-1][y + size-1] \
-                  - grid[x-1][y + size-1]        \
-                  - grid[x + size-1][y-1]        \
-                  + grid[x-1][y-1]
+                s = s_grid[x + size-1][y + size-1] \
+                  - s_grid[x-1][y + size-1]        \
+                  - s_grid[x + size-1][y-1]        \
+                  + s_grid[x-1][y-1]
                 if s > max_power:
                     max_power   = s
                     best_square = (x+1, y+1) # problem says grid is 1-indexed
                     best_size   = size
 
-    return "{},{},{}".format(*best_square, best_size)
+    if square_size:
+        return "{},{}".format(*best_square)
+    else:
+        return "{},{},{}".format(*best_square, best_size)
 
 # A 300x300 grid represents the power level of fuel cells. Each cell has a value
 # that depends on its x,y coordinates and the serial number (the file input).
@@ -70,5 +62,5 @@ if __name__ == '__main__':
     # init cells with fuel power formula
     grid = [ [ ((((X+10)*Y + SERIAL_NUMBER) * (X+10)) // 100) % 10 - 5 \
                for Y in range(1, 301) ] for X in range(1, 301) ]
-    print("PART ONE:", coordinates_max_power(grid))
-    print("PART TWO:", coordinates_max_power_any(grid))
+    print("PART ONE:", coordinates_max_power(grid, 3))
+    print("PART TWO:", coordinates_max_power(grid))
