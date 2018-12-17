23ff6bc6efcf2c0521a31b4877bc95e4821c9097
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-17 18:07:41 +0100

    solve part 1 in day06

--- day06/day06.py
+++ day06/day06.py
@@ -3,6 +3,55 @@
 import os
 import sys
 
+# Return the index of the place in `places` which is the closest to point (x,y).
+# Return None if there is a tie (2 or more places has the same distance)
+def index_closest_place(x, y, places):
+    min_d = 10**9
+    closest_place = []
+    for index, (px, py) in enumerate(places):
+        d = abs(px - x) + abs(py - y)
+        if d < min_d:
+            min_d = d
+            closest_place = [index]
+        elif d == min_d:
+            closest_place.append(index)
+    return closest_place[0] if len(closest_place) == 1 else None
+
+# Return the size of the finite area with the largest number of cells having the
+# same closest place.
+def largest_area_closest(places):
+    # Places are scattered all around an infinite grid. Each cell of this grid
+    # has 1 (or more) closest places (using the Manhattan distance). When a
+    # group of adjacent cells have the same closest place, they form an area.
+    # Since the grid is infinite, some area are also infinite (especially the
+    # ones in the corners). To know which area are finite, we first consider a
+    # basic grid, and then slightly expand this grid. Area with a finite size
+    # will have the same size, whereas the inifinite area will have a bigger
+    # size (because the grid is bigger). This function returns the largest area
+    # size which has a size independent of the size of the grid.
+    minx = min([x for x,y in places])
+    maxx = max([x for x,y in places])
+    miny = min([y for x,y in places])
+    maxy = max([y for x,y in places])
+
+    area_basic_grid = [0 for _ in places]
+    for x in range(minx, maxx+1):
+        for y in range(miny, maxy+1):
+            idx = index_closest_place(x, y, places)
+            if idx is not None:
+                area_basic_grid[idx] += 1
+
+    area_expanded_grid = [0 for _ in places]
+    s = 10
+    for x in range(minx - s, maxx+1 + s):
+        for y in range(miny - s, maxy+1 + s):
+            idx = index_closest_place(x, y, places)
+            if idx is not None:
+                area_expanded_grid[idx] += 1
+
+    finite_area = [a for a,b in zip(area_basic_grid, area_expanded_grid) if a == b]
+    return max(finite_area)
+
 # Input file is a list of point coordinates (x, y), one per line, representing
 # places in an infinite grid.
 #
@@ -19,3 +68,4 @@ if __name__ == '__main__':
          sys.exit("error: {} does not exist.".format(filename))
     places = [list(map(int, line.split(',')))
               for line in open(filename).read().splitlines()]
+    print("PART ONE:", largest_area_closest(places))
