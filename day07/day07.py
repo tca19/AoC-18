97d42fa62509c3fe42bd07d01ed97f2c8164107a
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-19 22:54:55 +0100

    add solution for part 1 of day07

--- day07/day07.py
+++ day07/day07.py
@@ -4,15 +4,45 @@ import os
 import sys
 from collections import defaultdict
 
-# Read filename, return a dict that maps a task to all the task requiring it.
+# Read filename, return 2 dicts: one to know the following steps of a step, the
+# other other one to know the preceding (= the requirements) of a step.
 def parse(filename):
-    tree = defaultdict(set)
+    following = defaultdict(set)
+    preceding = defaultdict(set)
     for line in open(filename).read().splitlines():
         line = line.strip().split()
         # line is like "Step T must be finished before step C can begin."
         # so the important information are line[1] and line[-3].
-        tree[line[1]].add(line[-3])
-    return tree
+        following[line[1]].add(line[-3])
+        preceding[line[-3]].add(line[1])
+    return following, preceding
+
+# Return the order in which the tasks should be completed.
+def completion_order(following, preceding):
+    # find the roots = the tasks that can start without requiring other tasks
+    all_tasks = set()
+    for k, v in following.items():
+        all_tasks.add(k)
+        all_tasks.update(v)
+    roots = all_tasks - set.union(*following.values())
+    steps_order = ""
+    done = set()
+    available = roots
+    while len(done) < len(all_tasks):
+        cur_task = sorted(list(available), reverse=True).pop()
+        steps_order += cur_task
+        done.add(cur_task)
+        available.remove(cur_task)
+        # remove cur_task from the task that needed it
+        for required in preceding.values():
+            if cur_task in required:
+                required.remove(cur_task)
+        # for all the task following cur_task, put the ones not done and that
+        # don't need any other requirement into the available stack
+        for next in following[cur_task]:
+            if next not in done and len(preceding[next]) == 0:
+                available.add(next)
+    return steps_order
 
 # Input file is a list of steps and requirements about which steps must be
 # finished before others can begin. Lines are like:
@@ -28,4 +58,5 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     if not os.path.exists(filename):
          sys.exit("error: {} does not exist.".format(filename))
-    tree = parse(filename)
+    following, preceding = parse(filename)
+    print("PART ONE:", completion_order(following, preceding))
