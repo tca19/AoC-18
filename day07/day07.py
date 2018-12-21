74e3927ee23d8fea6c000f60757228ada433b9e0
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-21 20:16:31 +0100

    add solution of part 2 in day07

--- day07/day07.py
+++ day07/day07.py
@@ -5,7 +5,7 @@ import sys
 from collections import defaultdict
 
 # Read filename, return 2 dicts: one to know the following steps of a step, the
-# other other one to know the preceding (= the requirements) of a step.
+# other one to know the preceding steps (= the requirements) of a step.
 def parse(filename):
     following = defaultdict(set)
     preceding = defaultdict(set)
@@ -19,7 +19,7 @@ def parse(filename):
 
 # Return the order in which the tasks should be completed.
 def completion_order(following, preceding):
-    # find the roots = the tasks that can start without requiring other tasks
+    # find the roots (= the tasks which can start without requiring other tasks)
     all_tasks = set()
     for k, v in following.items():
         all_tasks.add(k)
@@ -37,12 +37,54 @@ def completion_order(following, preceding):
         for required in preceding.values():
             if cur_task in required:
                 required.remove(cur_task)
-        # for all the task following cur_task, put the ones not done and that
-        # don't need any other requirement into the available stack
+        # for all the task following cur_task, put the ones not done and which
+        # don't need any other requirements into the available stack
         for next in following[cur_task]:
             if next not in done and len(preceding[next]) == 0:
                 available.add(next)
     return steps_order
+
+# Return the time needed to complete all steps if mutiple workers are available.
+def completion_time(following, preceding):
+    # stack is the list of steps which can start without requiring other steps
+    done, stack = set(), set(following.keys()) - set.union(*following.values())
+    n_workers   = 5
+    doing       = [None for _ in range(n_workers)] # what the worker #i is doing
+    time_left   = [0 for _ in range(n_workers)]    # time to complete its task
+    total_time  = 0
+    while len(stack) > 0 or sum(time_left) > 0:
+        # start as much as possible available tasks
+        stack = sorted(stack, reverse=True)
+        for i in range(n_workers):
+            if not stack: # test if stack is empty. If yes, no more tasks to add
+                break
+            if doing[i] is None: # worker #i is free, give him a task
+                task = stack.pop()
+                doing[i]     = task
+                time_left[i] = ord(task) - 4 # ord(task) - ord("A") + 1 + 60
+
+        # find the next completed task (and the time required to complete it)
+        d, pos = min((T,idx) for idx,T in enumerate(time_left) if T > 0)
+        done.add(doing[pos])
+        total_time += d
+
+        # suppose `d` seconds have happened (so running tasks have progressed)
+        for i in range(n_workers):
+            if time_left[i] > 0:
+                time_left[i] -= d
+
+        # task is done, remove it from the requirements of its following tasks.
+        # if it was the last requirement, add the following task to stack.
+        for next in following[doing[pos]]:
+            preceding[next].remove(doing[pos])
+            if len(preceding[next]) == 0:
+                stack.append(next)
+
+        # mark task as done
+        doing[pos] = None
+        time_left[pos] = 0
+
+    return total_time
 
 # Input file is a list of steps and requirements about which steps must be
 # finished before others can begin. Lines are like:
@@ -60,3 +102,5 @@ if __name__ == '__main__':
          sys.exit("error: {} does not exist.".format(filename))
     following, preceding = parse(filename)
     print("PART ONE:", completion_order(following, preceding))
+    following, preceding = parse(filename)
+    print("PART TWO:", completion_time(following, preceding))
