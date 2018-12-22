ea2e64ea3700c8d10a253f4aad757bfee5d752fb
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-22 22:54:41 +0100

    add soluion for part 2 in day09

--- day09/day09.py
+++ day09/day09.py
@@ -3,14 +3,14 @@
 import os
 import sys
 
-# class representing a node in a doubly linked list
+# Class representing a node in a doubly linked list.
 class Node:
     def __init__(self, val):
         self.prev = None
         self.next = None
         self.val  = val
 
-# insert a new node with value `val` AFTER node `self`. Return the new node.
+# Insert a new node with value `val` AFTER node `n`. Return the new node.
 def insert(n, val): # n <-> m becomes n <-> new <-> m
     new = Node(val)
     new.prev    = n
@@ -19,7 +19,7 @@ def insert(n, val): # n <-> m becomes n <-> new <-> m
     n.next      = new
     return new
 
-# move back by `offset` positions from `current` node in the list.
+# Move back by `offset` positions from `current` node in the list.
 def move_back(current, offset):
     for _ in range(offset):
         current = current.prev
@@ -28,7 +28,7 @@ def move_back(current, offset):
 # Return the score of the wininng player of the marble game. Use a doubly linked
 # list to represent the circle where the marbles are inserted.
 def winning_score(n_players, n_marbles):
-    scores = [0 for _ in range(n_players)]
+    scores = [0] * n_players
     current_player = 0
     current_node = Node(0)
     current_node.next = current_node
@@ -38,13 +38,12 @@ def winning_score(n_players, n_marbles):
             scores[current_player] += m
             current_node = move_back(current_node, 7)
             scores[current_player] += current_node.val
-            current_node.prev.next = current_node.next
+            current_node.prev.next = current_node.next # remove current node
             current_node.next.prev = current_node.prev
             current_node = current_node.next
         else:
             current_node = insert(current_node.next, m)
         current_player = (current_player + 1) % n_players
-
     return max(scores)
 
 # Input file is the number of players and the value of the last marble. The
@@ -53,7 +52,7 @@ def winning_score(n_players, n_marbles):
 # then 3...) at a specific position into a circle (a list) initialized with the
 # marble numbered 0. The insertion position is moved by one unit each turn. When
 # the value of the inserted marble is a multiple of 23, its value is added to
-# the current player score and the marble which is 7 units behing the inserting
+# the current player score and the marble which is 7 units behind the insertion
 # position is removed and also added to its score. The game ends when the last
 # marble is inserted. The winner is the player with the highest score.
 #
@@ -70,3 +69,4 @@ if __name__ == '__main__':
     n_players = int(line[0])
     n_marbles = int(line[-2])
     print("PART ONE:", winning_score(n_players, n_marbles))
+    print("PART TWO:", winning_score(n_players, n_marbles*100))
