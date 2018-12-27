cb9711a131120ed1fa7a2d605e950dcced0b6b2e
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-27 18:42:21 +0100

    add functions to parse input file and print map (day15)

--- day15/day15.py
+++ day15/day15.py
@@ -3,6 +3,40 @@
 import os
 import sys
 
+grid = []
+units = []
+
+class Unit:
+    def __init__(self, type, x, y):
+        self.type   = type
+        self.x      = x
+        self.y      = y
+        self.health = 200
+        self.attack = 3
+
+# Read the content of filename to get the unit positions and the map. Fill the
+# global variables `grid` (2D list) and `units` (list of Unit instances).
+def read_map(filename):
+    with open(filename) as f:
+        for i, line in enumerate(f):
+            grid.append(line.strip())
+            for j, c in enumerate(line):
+                if c in "EG":
+                    units.append(Unit(c, i, j))
+
+# Print the grid. For each line, displays the existing units and their health.
+def print_map():
+    # assume that units are sorted by position (top to bottom, left to right)
+    index = 0
+    for row in grid:
+        units_of_line = []
+        for cell in row:
+            print(cell, end="")
+            if cell in "EG":
+                units_of_line.append("{}({})".format(cell, units[index].health))
+                index += 1
+        print("  ", ", ".join(units_of_line))
+
 # Input file is a map representing Goblins (G) and Elves (E). The Goblins and
 # Elves take turns fighting. During its turn, each unit first move towards the
 # closest target then attack it. Each unit has 200 hit points (health) and an
@@ -27,3 +61,5 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     if not os.path.exists(filename):
          sys.exit("error: {} does not exist.".format(filename))
+    read_map(filename)
+    print_map()
