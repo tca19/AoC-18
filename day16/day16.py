074937faf0122ec77f153360c62ea84fe229b323
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-31 12:51:45 +0100

    run program instructions with opcodes mapping, solves part 2

--- day16/day16.py
+++ day16/day16.py
@@ -3,6 +3,7 @@
 import os
 import re
 import sys
+from collections import defaultdict
 
 def addr(register, A, B, C):
     new_register = list(register)
@@ -125,7 +126,38 @@ def possible_opcodes(before, instruction, after):
 def similar_different_opcodes(groups, n=3):
     opcodes_per_group = [possible_opcodes(before, instruction, after)
                          for before, instruction, after in groups]
-    return sum([1 for ops in opcodes_per_group if len(ops) >= n])
+    return len([1 for ops in opcodes_per_group if len(ops) >= n])
+
+# Return a dict that maps each opcode_id to an operation name.
+def find_opcode_mapping(groups):
+    # for each opcode (0 to 15), find the list of operations that are possible
+    choices = defaultdict(set)
+    for before, instruction, after in groups:
+        opcode_id, _, _, _ = instruction
+        choices[opcode_id].update(possible_opcodes(before, instruction, after))
+    mapping = {}
+    while len(mapping) < 16: # because 16 possible opcode, run until it's filled
+        # find ids which only have 1 possible operation. Put them in the mapping
+        # and remove these operations from the choice of other ids
+        for id, L in choices.items():
+            if len(L) == 1:
+                op_name = L.pop()
+                # OPCODES associate a str to a function
+                mapping[id] = OPCODES[op_name]
+                for k, v in choices.items():
+                    if op_name in v:
+                        v.remove(op_name)
+    return mapping
+
+# Run the program instruction, return the value in register 0 after execution.
+def run_program(groups, program):
+    mapping = find_opcode_mapping(groups)
+    registers = [0] * 4
+    # `program` is already splitted line by line
+    for opcode_id, A, B, C in program:
+        # mapping associates an id to a function
+        registers = mapping[opcode_id](registers, A, B, C)
+    return registers[0]
 
 # Input file has two sections:
 #   * the first one is composed of groups of 3 lines like:
@@ -156,3 +188,4 @@ if __name__ == '__main__':
          sys.exit("error: {} does not exist.".format(filename))
     groups, program = parse_data(filename)
     print("PART ONE:", similar_different_opcodes(groups))
+    print("PART TWO:", run_program(groups, program))
