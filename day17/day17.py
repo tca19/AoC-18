63c357f8ffcab2b7fc297ced0fed8751db72e121
tca19 <30314448+tca19@users.noreply.github.com>
2018-12-31 17:20:57 +0100

    add function to read and parse input file in day17

--- day17/day17.py
+++ day17/day17.py
@@ -1,13 +1,50 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import sys
+
+# Read `filename` (segment coordinates); return the grid representing containers
+def parse_data(filename):
+    x_min, x_max, y_min, y_max = 0, 0, 500, 0 # water source at (0,500)
+    vertical   = []
+    horizontal = []
+    extractor  = re.compile("-?\d+")
+    with open(filename) as f:
+        # lines are either "x=495, y=2..7" (vertical) or "y=7, x=495..501"
+        # (horizontal). The problem considers x is the column number, y the row
+        for line in f:
+            if line[0] == "x":
+                y, x_start, x_end = list(map(int, extractor.findall(line)))
+                x_min = min(x_min, x_start)
+                x_max = max(x_max, x_end)
+                y_min = min(y_min, y)
+                y_max = max(y_max, y)
+                vertical.append((y, x_start, x_end))
+            elif line[0] == "y":
+                x, y_start, y_end = list(map(int, extractor.findall(line)))
+                x_min = min(x_min, x)
+                x_max = max(x_max, x)
+                y_min = min(y_min, y_start)
+                y_max = max(y_max, y_end)
+                horizontal.append((x, y_start, y_end))
+    width  = y_max - y_min + 5
+    height = x_max - x_min + 5
+    grid = [ ["." for _ in range(width) ] for _ in range(height) ]
+    grid[0][500-y_min] = "+" # water source
+    for y, x_start, x_end in vertical:
+        for x in range(x_start, x_end+1):
+            grid[x-x_min][y-y_min] = "#"
+    for x, y_start, y_end in horizontal:
+        for y in range(y_start, y_end+1):
+            grid[x-x_min][y-y_min] = "#"
+    return grid
 
 # Input file is a list of coordinate ranges forming horizontal/vertical lines
 # (such as "x=495, y=2..7", a vertical line if y is the row and x the column).
-# These lines represent clay recipients, where water can be trapped. An infinite
+# These lines represent clay containers, where water can be trapped. An infinite
 # source of water at (500, 0) will produce water that will fall down, filling
-# the clay recipients. Below is an exemple of lines, and clay recipients (both
+# the clay containers. Below is an exemple of lines, and clay container (both
 # unfilled and filled with water).
 #
 #                                 ......+.......             ......+.......
@@ -33,3 +70,4 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     if not os.path.exists(filename):
          sys.exit("error: {} does not exist.".format(filename))
+    parse_data(filename)
