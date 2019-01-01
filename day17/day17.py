87c947ca0fe457b5d4897af8a58a0409364b9a07
tca19 <30314448+tca19@users.noreply.github.com>
2019-01-01 23:10:54 +0100

    make grid and x_min/x_max global variables in parse_data()

--- day17/day17.py
+++ day17/day17.py
@@ -4,41 +4,35 @@ import os
 import re
 import sys
 
-# Read `filename` (segment coordinates); return the grid representing containers
+SIZE = 2000 # assume maximum x and y will be 2000
+grid = [ ["." for _ in range(SIZE)] for _ in range(SIZE) ]
+x_min = y_min = SIZE
+x_max = y_max = 0
+
+# Read `filename` (segment coordinates); add lines in `grid`; find min/max
 def parse_data(filename):
-    x_min, x_max, y_min, y_max = 0, 0, 500, 0 # water source at (0,500)
-    vertical   = []
-    horizontal = []
     extractor  = re.compile("-?\d+")
+    global x_min, x_max, y_min, y_max
     with open(filename) as f:
         # lines are either "x=495, y=2..7" (vertical) or "y=7, x=495..501"
         # (horizontal). The problem considers x is the column number, y the row
         for line in f:
             if line[0] == "x":
                 y, x_start, x_end = list(map(int, extractor.findall(line)))
+                for x in range(x_start, x_end+1):
+                    grid[x][y] = "#"
                 x_min = min(x_min, x_start)
                 x_max = max(x_max, x_end)
                 y_min = min(y_min, y)
                 y_max = max(y_max, y)
-                vertical.append((y, x_start, x_end))
             elif line[0] == "y":
                 x, y_start, y_end = list(map(int, extractor.findall(line)))
+                for y in range(y_start, y_end+1):
+                    grid[x][y] = "#"
                 x_min = min(x_min, x)
                 x_max = max(x_max, x)
                 y_min = min(y_min, y_start)
                 y_max = max(y_max, y_end)
-                horizontal.append((x, y_start, y_end))
-    width  = y_max - y_min + 5
-    height = x_max - x_min + 5
-    grid = [ ["." for _ in range(width) ] for _ in range(height) ]
-    grid[0][500-y_min] = "+" # water source
-    for y, x_start, x_end in vertical:
-        for x in range(x_start, x_end+1):
-            grid[x-x_min][y-y_min] = "#"
-    for x, y_start, y_end in horizontal:
-        for y in range(y_start, y_end+1):
-            grid[x-x_min][y-y_min] = "#"
-    return grid
 
 # Input file is a list of coordinate ranges forming horizontal/vertical lines
 # (such as "x=495, y=2..7", a vertical line if y is the row and x the column).
@@ -71,3 +65,4 @@ if __name__ == '__main__':
     if not os.path.exists(filename):
          sys.exit("error: {} does not exist.".format(filename))
     parse_data(filename)
+    print_grid()
