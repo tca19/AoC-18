b0372d233f8355b040424ed6199928d5f7daafaf
tca19 <30314448+tca19@users.noreply.github.com>
2019-01-11 23:45:43 +0100

    add solution for part 1 (distinct constellation)

--- day25/day25.py
+++ day25/day25.py
@@ -1,8 +1,41 @@
-
 #!/usr/bin/env python3
 
 import os
 import sys
+
+# Return the Manhattan distance with 4-dimensional points x and y
+def distance(x, y):
+    return abs(x[0]-y[0]) + abs(x[1]-y[1]) + abs(x[2]-y[2]) + abs(x[3]-y[3])
+
+# Return if there is at least a point in L with a distance less than 3 to x
+def inside_constellation(x, L):
+    for p in L:
+        if distance(p, x) <= 3:
+            return True
+    return False
+
+# Return the number of distincts constellations formed by points
+def n_constellations(points):
+    constellations = {}
+    id = 0
+    for p in points:
+        possible_constellation = []
+        for i, L in constellations.items():
+            if inside_constellation(p, L):
+                possible_constellation.append(i)
+        if len(possible_constellation) == 0:
+            constellations[id] = [p]
+            id += 1
+        elif len(possible_constellation) == 1:
+            constellations[possible_constellation[0]].append(p)
+        else:
+            all_points = []
+            for i in possible_constellation:
+                all_points += constellations[i]
+                del constellations[i]
+            constellations[id] = all_points
+            id += 1
+    return len(constellations)
 
 # Input file is a list of 4-dimensional points, one per line. Two points having
 # a Manhattan distance less than 3 form a constellation. If a point has a
@@ -17,3 +50,5 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     if not os.path.exists(filename):
         sys.exit("error: {} does not exist.".format(filename))
+    points = [list(map(int, line.split(","))) for line in open(filename)]
+    print("PART ONE:", n_constellations(points))
