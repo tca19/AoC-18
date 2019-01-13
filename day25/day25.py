d5d11d77a3008f2b895142055a2b257df79b148c
tca19 <30314448+tca19@users.noreply.github.com>
2019-01-13 23:35:15 +0100

    optimize solution of part 1 in day25 with connected components

--- day25/day25.py
+++ day25/day25.py
@@ -2,40 +2,46 @@
 
 import os
 import sys
+from collections import defaultdict
 
-# Return the Manhattan distance with 4-dimensional points x and y
+# Return the Manhattan distance between the 4-dimensional points x and y
 def distance(x, y):
     return abs(x[0]-y[0]) + abs(x[1]-y[1]) + abs(x[2]-y[2]) + abs(x[3]-y[3])
 
 # Return if there is at least a point in L with a distance less than 3 to x
 def inside_constellation(x, L):
-    for p in L:
-        if distance(p, x) <= 3:
+    for point in L:
+        if distance(point, x) <= 3:
             return True
     return False
 
 # Return the number of distincts constellations formed by points
 def n_constellations(points):
-    constellations = {}
-    id = 0
-    for p in points:
-        possible_constellation = []
-        for i, L in constellations.items():
-            if inside_constellation(p, L):
-                possible_constellation.append(i)
-        if len(possible_constellation) == 0:
-            constellations[id] = [p]
-            id += 1
-        elif len(possible_constellation) == 1:
-            constellations[possible_constellation[0]].append(p)
-        else:
-            all_points = []
-            for i in possible_constellation:
-                all_points += constellations[i]
-                del constellations[i]
-            constellations[id] = all_points
-            id += 1
-    return len(constellations)
+    # create an undirected graph where vertices are the points and two vertices
+    # are connected by an edge if the distance between the points is less than 3
+    edges = defaultdict(set)
+    for i in range(len(points)):
+        for j in range(i+1, len(points)):
+            if distance(points[i], points[j]) <= 3:
+                edges[i].add(j)
+                edges[j].add(i)
+    # number of distincts constellations = number of connected components
+    n_connected = 0
+    seen = set()
+    for i in range(len(points)):
+        if i in seen:
+            continue
+        n_connected += 1
+        # go through all the neighbors of i, mark them as seen
+        stack = [i]
+        while stack:
+            node = stack.pop()
+            if node in seen:
+                continue
+            seen.add(node)
+            for neighbor in edges[node]:
+                stack.append(neighbor)
+    return n_connected
 
 # Input file is a list of 4-dimensional points, one per line. Two points having
 # a Manhattan distance less than 3 form a constellation. If a point has a
